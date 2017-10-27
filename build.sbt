846ed52a1e930ff214deba401d5a368d9d7ee5af
Ben Black <benblack@tumblr.com>
2017-10-27 12:37:23 -0400

    Init

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "colossus-test"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
+
+libraryDependencies ++= Seq(
+  "com.tumblr"     %% "colossus"        % "0.10.1",
+  "ch.qos.logback" %  "logback-classic" % "1.2.2"
+)
---
+++ src/main/scala/MyServer.scala
@@ -0,0 +1,35 @@
+import akka.actor.ActorSystem
+import colossus.IOSystem
+import colossus.protocols.http.{Http, HttpRequest}
+import colossus.protocols.http.HttpMethod.Get
+import colossus.protocols.http.UrlParsing.{Root, on}
+import colossus.protocols.http.{HttpServer, Initializer, RequestHandler}
+import colossus.service.GenRequestHandler.PartialHandler
+
+
+object MyServer extends App {
+
+  implicit val actorSystem = ActorSystem()
+  implicit val ioSystem = IOSystem()
+
+  HttpServer.start("example-server", 9000) { initContext =>
+    new Initializer(initContext) {
+
+      val httpClient = Http.client("google.com", 80)
+
+      override def onConnect = serverContext => new RequestHandler(serverContext) {
+        override def handle: PartialHandler[Http] = {
+          case request@Get on Root =>
+            val asyncResult = httpClient.send(HttpRequest.get("/#q=mysearch"))
+            asyncResult.map { response =>
+              val body = response.body.bytes.utf8String
+              request.ok(body)
+            }
+        }
+      }
+    }
+  }
+
+}
+
+
